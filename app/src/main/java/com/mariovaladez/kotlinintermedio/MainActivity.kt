89e2cf2742bd1be558e6bae78a606480fc2993ad
Íkaro Chacal Guerrero <ikarochacalguerrero@gmail.com>
2022-05-09 17:46:17 -0500

    ejercicios enum: cartas, calculadora, dias de la semana

--- app/src/main/java/com/mariovaladez/kotlinintermedio/MainActivity.kt
+++ app/src/main/java/com/mariovaladez/kotlinintermedio/MainActivity.kt
@@ -9,7 +9,10 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         //Lección 1 Enum Classes
-        enumClases()
+        //enumClases()
+        //RepartirCartas()
+        //calcular()
+        diasDeLaSemana()
     }
 
     enum class Direccion(val dir : Int){
@@ -50,12 +53,79 @@ class MainActivity : AppCompatActivity() {
         //Inicialización
         println(userDirection.dir)
 
+    }
 
+    /////////////////////////////////JUEGO DE CARTAS///////////////////////////
 
-
-
+    enum class PaloNaipe {DIAMANTES, TREBOLES, CORAZONES, PICAS}
+    class Carta( val carta: PaloNaipe, val valor : Int){
+        fun mostrar(){
+            println("Mario $valor de $carta")
+        }
 
     }
+    fun RepartirCartas(){
+        fun getCarta () = PaloNaipe.values().random()
+        fun getValor () = (1..12).random()
+        for(i in 1..100){
+            Carta(getCarta(),getValor()).mostrar()
+
+        }
+    }
+
+    /////////////////////////CALCULADORA///////////////////////////////////////////
+
+
+enum class operacion(val tipo : String ){
+    SUMA("+"),
+    RESTA("-"),
+    MULTIPLICACION("*"),
+    DIVISION("/")
+
+}
+    class Calculo(val valor1: Int, val valor2 : Int, val operation : operacion){
+       fun calcule(){
+           val resultado = when(operation){
+               operacion.SUMA -> valor1+valor2
+               operacion.RESTA -> valor1-valor2
+               operacion.MULTIPLICACION -> valor1*valor2
+               operacion.DIVISION -> valor1/valor2
+
+
+           }
+           println("$valor1 ${operation.tipo} $valor2  es igual a $resultado")
+       }
+
+    }
+private fun calcular(){
+    val calculo1 = Calculo(20,30,operacion.SUMA)
+    calculo1.calcule()
+    val calculo2 = Calculo(20,30,operacion.RESTA)
+    calculo2.calcule()
+    val calculo3 = Calculo(20,30,operacion.MULTIPLICACION)
+    calculo3.calcule()
+    val calculo4 = Calculo(20,30,operacion.DIVISION)
+    calculo4.calcule()
+
+}
+    ////////////////////////////////////////////DIAS DE LA SEMANA////////////////////////////////////////////////
+    enum class Dias(val dia: Int){
+        LUNES(1),
+        MARTES(2),
+        MIERCOLES(3),
+        JUEVES(4),
+        VIERNES(5),
+        SABADO(6),
+        DOMINGO(7)
+
+    }
+private fun diasDeLaSemana(){
+    for(dia in Dias.values()){
+       val diaSemana = Dias.valueOf(dia.name)
+        println("${diaSemana.dia} "+"$diaSemana")
+    }
+
+}
 
 
 }
